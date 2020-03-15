41119b1b81728829a9ac56f9e9383b615d096fa4
aki.a <yellow_a2452@yahoo.co.jp>
2020-03-15 16:42:24 +0900

    login page

--- js/main.js
+++ js/main.js
@@ -1,52 +1,24 @@
-var items = [
-  {
-    name: 'えんぴつ',
-    price: 300,
-    quantity: 0
+Vue.component('user-login', {
+  template: '#login-template',
+  data: function () {
+    return {
+      userid: '',
+      passward: ''
+    }
   },
-  {
-    name: 'ノート',
-    price: 400,
-    quantity: 0
-  },
-  {
-    name: '消しゴム',
-    price: 500,
-    quantity: 0
+  methods: {
+    login: function () {
+      auth.login(this.userid, this.passward);
+    }
   }
-]
+})
+var auth = {
+  login: function (id, pass) {
+    window.alert("userid: " + id + "\n" + "passward: " + pass);
+  }
+}
 
-var vm = new Vue({
-  el: '#app',
-  data: {
-    items: items
-  },
-  filters: {
-    numberWithDelimiter: function (value) {
-      if (!value) {
-        return '0'
-      }
-      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1')
-    }
-  },
-  computed: {
-    totalPrice: function () {
-      return this.items.reduce(function (sum, item) {
-        return sum + (item.price * item.quantity)
-      }, 0)
-    },
-    totalPriceWithMax: function () {
-      return Math.floor(this.totalPrice * 1.08)
-    },
-    canBuy: function () {
-      return this.totalPrice >= 1000
-    },
-    errorMessageStyle: function () {
-      return {
-        border: this.canBuy ? '' : '1px solid red',
-        color: this.canBuy ? '' : 'red'
-      }
-    }
-  }
+new Vue({
+  el: "#login-example"
 });
 
